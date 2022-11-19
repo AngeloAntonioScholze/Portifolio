b4afa1582a51a39f2dfbd87c99686b5ce8a8efa1
Angelo Antonio Scholze <antonioscholze@gmail.com>
2022-11-19 03:45:03 -0300

    starting code for portifolio, and starting flutter flame

--- relogio/index.js
+++ relogio/index.js
@@ -1,1 +1,16 @@
-console.log('teste');
+var maximum69Number  = function(num) {
+    var numToString = num.toString();
+    for (let i = 0; i < numToString.length; i++) {
+        console.log(numToString[i]);
+        if(numToString[i] == 6) {
+            var maxValue = numToString.replaceAt(i, '9');
+            return maxValue;
+        }
+    }
+    return null;
+};
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+var result = maximum69Number(9669);
+console.log(result);
