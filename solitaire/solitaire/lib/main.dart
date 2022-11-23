0fbb436ece33e57feed7661f0387861d6384cf32
Angelo Scholze <52944843+AngeloAntonioScholze@users.noreply.github.com>
2022-11-23 01:23:14 -0300

    Delete solitaire/solitaire directory

--- solitaire/solitaire/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flame/game.dart';
-import 'package:flutter/widgets.dart';
-import 'package:solitaire/solitaire_game.dart';
-
-void main() {
-  final game = SolitaireGame();
-  runApp(GameWidget(game: game));
-}
